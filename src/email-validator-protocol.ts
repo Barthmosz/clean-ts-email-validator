eafd067f169a8b5c83e324f6ee62400da1e56d9e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 01:23:01 -0300

    feat: add Validator protocol to sut type

---
+++ src/email-validator-protocol.ts
@@ -0,0 +1,3 @@
+export interface Validator {
+    validate: (email: string) => boolean
+}
--- src/email-validator.spec.ts
+++ src/email-validator.spec.ts
@@ -1,20 +1,26 @@
 import { EmailValidator } from './email-validator'
+import { type Validator } from './email-validator-protocol'
+
+const makeSut = (): Validator => {
+    const sut = EmailValidator
+    return sut
+}
 
 describe('EmailValidator', () => {
     test('should return false if an invalid email is provided', () => {
-        const sut = EmailValidator
+        const sut = makeSut()
         const isValid = sut.validate('@mail.com')
         expect(isValid).toBe(false)
     })
 
     test('should return false if an empty email is provided', () => {
-        const sut = EmailValidator
+        const sut = makeSut()
         const isValid = sut.validate('')
         expect(isValid).toBe(false)
     })
 
     test('should return true if a valid email is provided', () => {
-        const sut = EmailValidator
+        const sut = makeSut()
         const isValid = sut.validate('valid_email@mail.com')
         expect(isValid).toBe(true)
     })
