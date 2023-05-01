b0cea271421ad12e5558198b1019f27edf1275bb
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 01:13:28 -0300

    refactor: adjust test to use static method

--- src/email-validator.spec.ts
+++ src/email-validator.spec.ts
@@ -2,19 +2,19 @@ import { EmailValidator } from './email-validator'
 
 describe('EmailValidator', () => {
     test('should return false if an invalid email is provided', () => {
-        const sut = new EmailValidator()
+        const sut = EmailValidator
         const isValid = sut.validate('@mail.com')
         expect(isValid).toBe(false)
     })
 
     test('should return false if an empty email is provided', () => {
-        const sut = new EmailValidator()
+        const sut = EmailValidator
         const isValid = sut.validate('')
         expect(isValid).toBe(false)
     })
 
     test('should return true if a valid email is provided', () => {
-        const sut = new EmailValidator()
+        const sut = EmailValidator
         const isValid = sut.validate('valid_email@mail.com')
         expect(isValid).toBe(true)
     })
