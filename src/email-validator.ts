20837cfd7f4d023c6c3c29de257d6f400310426c
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-01 01:12:24 -0300

    refactor: change validate method to static

--- src/email-validator.ts
+++ src/email-validator.ts
@@ -4,7 +4,7 @@ export class EmailValidator {
      * @param {string} email
      * @returns {boolean}
     */
-    validate(email: string): boolean {
+    static validate(email: string): boolean {
         const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g
         return emailRegex.test(email)
     }
